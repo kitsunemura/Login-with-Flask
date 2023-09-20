67b09841c70f250f85c8ea82ec633846e5d23814
Byron Rodriguez <byron303@gmail.com>
2023-09-20 21:07:06 +0000

    subiendo tarea

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -21,7 +21,6 @@ export const Private = (props) => {
       <div className="container h-100">
         <div className="row h-100">
           <div className="col-12">
-            {/* Conditionally render the welcome message */}
             {isLoggedIn ? (
               <>
                 <h1>
